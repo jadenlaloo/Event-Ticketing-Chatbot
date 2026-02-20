8735dd0143532c5bd41f90d00b508524db770b21
Jaden Wandame Laloo <jadenwandame@karunya.edu.in>
2026-02-20 14:47:45 +0530

    Switch to icons new folder

--- app.py
+++ app.py
@@ -293,7 +293,7 @@ def get_image_base64(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode()
 
-icons_folder = "icons"
+icons_folder = "icons new"
 icon_files = [
     "books.jpeg",
     "download (1).jpeg", 
